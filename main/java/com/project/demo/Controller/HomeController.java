85e52329686578454c0ac3ba60e488ed7f8a7e5b
Amrutha R <32715521+amrutha24@users.noreply.github.com>
2024-10-04 14:15:58 +0530

    Add files via upload

---
+++ main/java/com/project/demo/Controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.project.demo.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.project.demo.Service.CoronaDataService;
+import com.project.demo.model.LocationStates;
+
+@Controller
+public class HomeController 
+{
+	@Autowired
+	CoronaDataService crnService;
+	@GetMapping("/")
+	public String home(Model model)
+	{
+		List<LocationStates> allstates=crnService.getAllstates();
+		int totalDeathsReported=allstates.stream().mapToInt(stat->stat.getLatestTotalDeaths()).sum();
+		model.addAttribute("LocationStates",allstates);
+		model.addAttribute("totalDeathsReported",totalDeathsReported);
+		return "home";
+	}
+
+}
